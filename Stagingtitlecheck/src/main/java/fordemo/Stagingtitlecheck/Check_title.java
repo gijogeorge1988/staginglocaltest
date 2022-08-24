2d7a53a3dc251d88ff896e6f84c86614a5b81281
Qbuser <gijo@qburst.com>
2022-08-24 12:12:46 +0530

    d11

--- Stagingtitlecheck/src/main/java/fordemo/Stagingtitlecheck/Check_title.java
+++ Stagingtitlecheck/src/main/java/fordemo/Stagingtitlecheck/Check_title.java
@@ -26,7 +26,7 @@ public class Check_title {
 	{
 		driver.get("http://15.206.147.106/");
 		String str=driver.getTitle();
-		Assert.assertEquals(str,"CDCI Demoo");
+		Assert.assertEquals(str,"CDCI Demo");
 		System.out.println(str);
 		
 	}

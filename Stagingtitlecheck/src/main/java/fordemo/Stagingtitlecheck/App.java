f756e098d1809736b1d8bc6f99c5017f1c28dafb
Qbuser <gijo@qburst.com>
2022-08-24 09:54:02 +0530

    first

---
+++ Stagingtitlecheck/src/main/java/fordemo/Stagingtitlecheck/App.java
@@ -0,0 +1,13 @@
+package fordemo.Stagingtitlecheck;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ Stagingtitlecheck/src/main/java/fordemo/Stagingtitlecheck/Check_title.java
@@ -0,0 +1,35 @@
+package fordemo.Stagingtitlecheck;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.testng.Assert;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+public class Check_title {
+	
+	public WebDriver driver;
+	@BeforeTest
+	public void load()
+	{
+		ChromeOptions options = new ChromeOptions();
+		options.addArguments("--headless");
+		driver = new ChromeDriver(options);
+	    System.setProperty("webdriver.chrome.driver", "/usr/bin/chromedriver");
+	    
+		
+	}
+	
+	@Test
+	public void check()
+	{
+		driver.get("http://3.111.53.55/");
+		String str=driver.getTitle();
+		Assert.assertEquals(str,"Jenksinss");
+		System.out.println(str);
+		
+	}
+	   
+
+}
